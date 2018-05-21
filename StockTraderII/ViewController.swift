ff65ffbbd9ad52299c562ac9961ca5adb81cdff4
francisc11219 <25981779+francisc112@users.noreply.github.com>
2018-05-21 12:07:53 -0500

    Initial Commit

---
+++ StockTraderII/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StockTraderII
+//
+//  Created by Francisco on 21/05/2018.
+//  Copyright © 2018 Francisco Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
